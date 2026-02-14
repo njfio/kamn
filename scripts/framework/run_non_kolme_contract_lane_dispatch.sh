01c77dcb1c74045e9ec312ac3d2d1eccaa7c59ce
Nicholas Ferguson <me@njf.io>
2026-02-13 20:03:10 -0500

    refactor(ci): migrate runtime live-network pilot deep lane to dispatcher manifest (#2878) (#2879)

--- scripts/framework/run_non_kolme_contract_lane_dispatch.sh
+++ scripts/framework/run_non_kolme_contract_lane_dispatch.sh
@@ -134,6 +134,7 @@ resolve_manifest_name() {
     run_invariant_fuzz_concurrency_contract_lane.sh) echo "runtime_invariant_fuzz_concurrency_contract_lane.json" ;;
     run_lifecycle_property_contract_lane.sh) echo "runtime_lifecycle_property_contract_lane.json" ;;
     run_live_network_partition_reconnect_contract_lane.sh) echo "runtime_live_network_partition_reconnect_contract_lane.json" ;;
+    run_live_network_pilot_deep_lane.sh) echo "runtime_live_network_pilot_deep_lane.json" ;;
     run_live_network_pilot_deep_contract_lane.sh) echo "runtime_live_network_pilot_deep_contract_lane.json" ;;
     run_live_network_smoke_lane.sh) echo "runtime_live_network_smoke_lane.json" ;;
     run_live_network_smoke_contract_lane.sh) echo "runtime_live_network_smoke_contract_lane.json" ;;
@@ -158,6 +159,7 @@ resolve_phase_name() {
     run_live_transport_replay_tamper_deep_lane.sh) echo "run" ;;
     run_live_transport_smoke_parity_lane.sh) echo "run" ;;
     run_live_network_smoke_lane.sh) echo "run" ;;
+    run_live_network_pilot_deep_lane.sh) echo "run" ;;
     run_quorum_attestation_replay_guard_lane.sh) echo "run" ;;
     run_governance_lifecycle_rollback_lane.sh) echo "run" ;;
     run_dashboard_shell_determinism_matrix_lane.sh) echo "run" ;;
