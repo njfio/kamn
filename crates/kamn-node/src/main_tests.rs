fffdb37314202c181f5762d76a14cedea3e02235
Nicholas Ferguson <me@njf.io>
2026-02-13 20:24:14 -0500

    feat(node): add tokio async runtime entrypoint bridge (#2891) (#2990)

--- crates/kamn-node/src/main_tests.rs
+++ crates/kamn-node/src/main_tests.rs
@@ -204,6 +204,7 @@ fn spawn_kolme_live_mock_server(replies: Vec<MockHttpReply>) -> (String, Arc<Mut
     (format!("http://{addr}"), recorded_requests)
 }
 
+mod async_runtime_contract_tests;
 mod cli_contract_tests;
 mod core_behavior_tests;
 mod observability_endpoint_tests;
