789c3eb55293d9f6d42fa83ca3d63b0a852fd3cf
n <me@njf.io>
2026-03-18 16:20:58 -0400

    refactor(7011): tighten solana asset movement support exports

--- crates/kamn-node/src/main_tests/service_api_endpoint_tests/task_escrow_persistence_contract_tests/support.rs
+++ crates/kamn-node/src/main_tests/service_api_endpoint_tests/task_escrow_persistence_contract_tests/support.rs
@@ -16,11 +16,10 @@ pub(super) use request_support::{
     release_escrow,
 };
 pub(super) use solana_asset_movement_support::{
-    assert_persisted_solana_signature_metadata, assert_released_escrow_has_solana_signature_metadata,
-    build_asset_movement_harness, build_live_solana_asset_movement_context,
-    build_solana_settlement_fixture, cleanup_solana_settlement_fixture, cleanup_state_file,
-    fund_and_release_live_escrow, fund_live_escrow, release_live_escrow_across_restart,
-    release_live_escrow_twice, settlement_tx_signature,
+    assert_persisted_solana_signature_metadata,
+    assert_released_escrow_has_solana_signature_metadata,
+    build_live_solana_asset_movement_context, fund_and_release_live_escrow,
+    release_live_escrow_across_restart, release_live_escrow_twice, settlement_tx_signature,
 };
 pub(super) use state_support::{
     build_task_escrow_snapshot, set_state_file_env, unique_named_state_file,
